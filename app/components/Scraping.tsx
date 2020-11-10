8749622004ca62887348bd1468798d744c3397be
Johannes Filter <hi@jfilter.de>
2020-11-10 18:35:35 +0100

    basic scraping setup

--- app/components/Scraping.tsx
+++ app/components/Scraping.tsx
@@ -1,16 +1,50 @@
 import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import BrowserView from 'react-electron-browser-view';
 import routes from '../constants/routes.json';
 // import styles from './Home.css';
 
-import BrowserView from 'react-electron-browser-view';
-
 import Base from './Base';
+
+const scrapingConfigYoutube = {
+  loginUrl: 'https://www.youtube.com/account',
+  logingCookie: 'LOGIN_INFO',
+};
+
+// the react wrapper is buggy, remove it?
+// https://github.com/vantezzen/react-electron-browser-view
+
+// the api
+// https://www.electronjs.org/docs/api/web-contents
 
 export default function Scraping(): JSX.Element {
   const [curUrl, setCurUrl] = useState(null);
   const [curHtml, setCurHtml] = useState('');
-  const browser = useRef(null);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const browser = useRef<any>(null);
+
+  const waitUntilLoggingIn = async () => {
+    const webContents = browser?.current.view.webContents;
+    const cookies = await webContents.session.cookies.get({});
+    // complexity is currently not needed, maybe later?
+    const isLoggedIn = cookies.some(
+      (x: any) => x.name === scrapingConfigYoutube.logingCookie
+    );
+    setUserLoggedIn(isLoggedIn);
+    return isLoggedIn;
+  };
+
+  const goToStart = () => {
+    browser.current.loadURL(scrapingConfigYoutube.loginUrl, {
+      userAgent: 'Chrome',
+    });
+  };
+
+  const clearBrowser = () => {
+    browser?.current.view.webContents.session.clearStorageData();
+    goToStart();
+  };
+
   return (
     <Base>
       <h2 className="title is-2">Scraping</h2>
@@ -20,29 +54,43 @@ export default function Scraping(): JSX.Element {
         perferendis sequi libero laborum esse soluta, ut rerum placeat
         consectetur deleniti.
       </p>
+      <br />
+      <button className="button" type="button" onClick={clearBrowser}>
+        Reset Browser
+      </button>
+      <br />
+      {!userLoggedIn && <p>Please login before continuing.</p>}
+      {userLoggedIn && (
+        <button className="button" type="button">
+          start scraping
+        </button>
+      )}
 
-      <h3 className="title is-3">Working on {curUrl}</h3>
-
-      <p>{curHtml.substring(0, 100)}</p>
+      {/* <h3 className="title is-3">Working on {curUrl}</h3> */}
 
       <BrowserView
         ref={browser}
-        src="https://www.youtube.com"
-        // Using events
-        onDidAttach={() => {
-          console.log('BrowserView attached');
+        src="about:blank"
+        style={{
+          height: 500,
         }}
-        onUpdateTargetUrl={() => {
-          const url = browser.current.getURL();
+        onDidAttach={() => {
+          // can't set custom userAgent with a prop
+          setTimeout(goToStart, 100);
+        }}
+        onUpdateTargetUrl={async () => {
+          if (!userLoggedIn) {
+            const justLoggedIn = await waitUntilLoggingIn();
+            if (!justLoggedIn) return;
+          }
+
+          const url = browser?.current.getURL();
           setCurUrl(url);
 
           browser.current
             .executeJavaScript('document.documentElement.innerHTML')
-            .then(setCurHtml);
-        }}
-        // Providing styling for the container element
-        style={{
-          height: 500,
+            .then(setCurHtml)
+            .catch((x) => console.error(x));
         }}
       />
     </Base>
