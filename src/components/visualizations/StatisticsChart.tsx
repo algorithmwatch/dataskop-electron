706114a7d5502736b8e03402c80d411e979a286b
Johannes Filter <hi@jfilter.de>
2021-05-10 13:31:56 +0200

    add more statistics charts

--- src/components/visualizations/StatisticsChart.tsx
+++ src/components/visualizations/StatisticsChart.tsx
@@ -1,12 +1,19 @@
+import _ from 'lodash';
 import React from 'react';
-import { VictoryAxis, VictoryBar, VictoryChart } from 'victory';
+import {
+  VictoryAxis,
+  VictoryBar,
+  VictoryChart,
+  VictoryHistogram,
+} from 'victory';
+import { ScrapingResult } from '../../db/types';
 
-function DsBarChar({ data, title }) {
+function DsBarChart({ data, title, x, y }) {
   return (
-    <div className="h-1/3">
+    <div>
       <h4>{title}</h4>
       <VictoryChart
-        // theme={VictoryTheme.material}
+        padding={{ left: 100, bottom: 100 }}
         domainPadding={{ x: 10 }}
         // width={200}
         // height={200}
@@ -21,19 +28,31 @@ function DsBarChar({ data, title }) {
           }
         />
         <VictoryAxis
-          style={
-            {
-              // tickLabels: { fontSize: 5 },
-            }
-          }
+          style={{
+            tickLabels: { fontSize: 10 },
+          }}
         />
         <VictoryBar
           horizontal
           barRatio={0.9}
           style={{ data: { fill: '#c43a31' } }}
           data={data}
-          y="count"
-          x="id"
+          y={y}
+          x={x}
+        />
+      </VictoryChart>
+    </div>
+  );
+}
+
+function DsHistogram({ data, field }) {
+  return (
+    <div>
+      <h4>{field}</h4>
+      <VictoryChart domainPadding={10}>
+        <VictoryHistogram
+          style={{ data: { fill: '#c43a31' } }}
+          data={data.map((row) => ({ x: row.fields[field] }))}
         />
       </VictoryChart>
     </div>
@@ -53,23 +72,38 @@ export default function StatisticsChart({
     .map((x) => x.fields.recommendedVideos)
     .flat();
 
-  console.log(onlyRecVideos);
-
   const freqRecIds = Object.entries(_.countBy(onlyRecVideos, 'id')).map((x) => {
     return { id: x[0], count: x[1] };
   });
-
   const maxFreqRecIds = _.sortBy(freqRecIds, ['count']).reverse().slice(0, 10);
 
-  console.log(maxFreqRecIds);
+  const freqCreators = Object.entries(
+    _.countBy(onlyRecVideos, 'channelName'),
+  ).map((x) => {
+    return { creator: x[0], count: x[1] };
+  });
+  const maxCreators = _.sortBy(freqCreators, ['count']).reverse().slice(0, 10);
+  const availMetrics = ['upvotes', 'downvotes', 'duration', 'viewCount'];
 
   return (
     <>
-      <DsBarChar data={maxFreqRecIds} title="Most Recommended Video" />
-      {/* <Chart
-        data={data.filter((x) => x.success && x.slug.includes('video-page'))}
-        visType={visType}
-      /> */}
+      <div className="grid grid-cols-2">
+        <DsBarChart
+          data={maxFreqRecIds}
+          title="Most Recommended Video"
+          x="id"
+          y="count"
+        />
+        <DsBarChart
+          data={maxCreators}
+          title="Most Recommended Creators"
+          x="creator"
+          y="count"
+        />
+        {availMetrics.map((x) => (
+          <DsHistogram key={x} data={onlyVideos} field={x} />
+        ))}
+      </div>
     </>
   );
 }
