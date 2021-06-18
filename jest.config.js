323162f8e243063a5e1c279c3005e208c84c4c67
Johannes Filter <hi@jfilter.de>
2021-06-18 16:08:12 +0200

    fix jest for new ESM modules

--- jest.config.js
+++ jest.config.js
@@ -18,12 +18,16 @@ const config = {
     // lowdb is a ESM module and this causes some problems right now.
     // To make it work with jest, import the .js files directly.
     lowdb: 'lowdb/lib/index',
+    // another ESM module
+    'p-queue': 'p-queue/dist/index',
   },
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
   moduleDirectories: ['node_modules', 'src/node_modules'],
   setupFiles: ['./.erb/scripts/CheckBuildsExist.js'],
-  // also transform our TS-only package harke and the ESM module lowdb
-  transformIgnorePatterns: ['node_modules/(?!(@algorithmwatch/harke|lowdb))'],
+  // also transform our TS-only package harke and the ESM module lowdb (and others)
+  transformIgnorePatterns: [
+    'node_modules/(?!(@algorithmwatch/harke|lowdb|p-queue|p-timeout))',
+  ],
   // only throw warnings for TS problems
   globals: {
     'ts-jest': {
