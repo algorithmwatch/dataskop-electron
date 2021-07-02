6078e0cfe528d13c5d5c04dada5d8730a508e696
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2021-07-02 15:17:32 +0200

    ScrapingProgressBar WIP

--- src/components/ScrapingProgressBar.tsx
+++ src/components/ScrapingProgressBar.tsx
@@ -7,8 +7,10 @@ export default function ScrapingProgressBar() {
   const {
     state: {
       scrapingConfig,
+      visibleWindow,
       scrapingProgress: { isActive, value, step },
     },
+    dispatch,
   } = useScraping();
   // if (!isActive) {
   //   return null;
@@ -18,28 +20,28 @@ export default function ScrapingProgressBar() {
     action: 'Überprüfe Traktorstrahl…',
     profile: 'Starte den Laser…',
     video: 'Überprüfe Druckkabine…',
-    search: 'Überprüfe Hyperraumantrieb…',
+    search: 'Starte Hyperraumantrieb…',
   };
   const currentType = scrapingConfig.steps[step].type;
   const description = typeDescriptionMap[currentType];
 
   return (
-    <div className="max-w-min h-7 relative flex items-center bg-yellow-300">
-      <div className="z-10 flex items-center px-2 space-x-1.5">
-        <FontAwesomeIcon
-          icon={faSpinnerThird}
-          spin
-          size="sm"
-          className="text-yellow-1400"
-        />
-        <div className="z-10 relative text-sm text-yellow-1200 whitespace-nowrap pr-3">
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+    <div
+      className="max-w-min h-10 relative flex items-center cursor-pointer group"
+      onClick={() =>
+        dispatch({ type: 'set-visible-window', visibleWindow: !visibleWindow })
+      }
+    >
+      <div className="z-20 flex items-center px-2 space-x-2 text-yellow-1300 group-hover:text-yellow-1100">
+        <FontAwesomeIcon icon={faSpinnerThird} spin size="sm" className="" />
+        <div className="relative text-sm whitespace-nowrap pr-3">
           {description}
         </div>
       </div>
-      <div
-        className="z-0 absolute inset-y-0 left-0 bg-yellow-600"
-        style={{ width: '30%' }}
-      />
+      <div className="z-0 absolute bottom-0 inset-x-0 h-1 bg-yellow-300">
+        <div className="bg-yellow-700 h-full" style={{ width: '30%' }} />
+      </div>
     </div>
   );
 }
