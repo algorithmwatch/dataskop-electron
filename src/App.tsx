708421e30d931046032110d93b9792229ce4e6ca
Christopher Pietsch <cpietsch@gmail.com>
2021-07-08 14:47:51 +0200

    reverted app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ import DonationPage1 from './pages/DonationPage1';
 import DonationPage2 from './pages/DonationPage2';
 import DonationSuccessPage from './pages/DonationSuccessPage';
 import InterfaceTutorialPage from './pages/InterfaceTutorialPage';
-// import IntroductionPage from './pages/IntroductionPage';
+import IntroductionPage from './pages/IntroductionPage';
 import MyDataHintPage from './pages/MyDataHintPage';
 import OnboardingPage1 from './pages/OnboardingPage1';
 import OnboardingPage2 from './pages/OnboardingPage2';
@@ -35,7 +35,6 @@ import StartPage from './pages/StartPage';
 import VisualizationAutoplayChainPage from './pages/VisualizationAutoplayChainPage';
 import VisualizationNewsPage from './pages/VisualizationNewsPage';
 import VisualizationProfilePage from './pages/VisualizationProfilePage';
-import VisualizationProfilePageDummy from './pages/VisualizationProfilePageDummy';
 import VisualizationSearchPage from './pages/VisualizationSearchPage';
 
 export default function App() {
@@ -114,13 +113,9 @@ export default function App() {
                 />
                 <Route path={routes.ONBOARDING_2} component={OnboardingPage2} />
                 <Route path={routes.ONBOARDING_1} component={OnboardingPage1} />
-                {/* <Route
-                  path={routes.INTRODUCTION}
-                  component={IntroductionPage}
-                /> */}
                 <Route
                   path={routes.INTRODUCTION}
-                  component={VisualizationProfilePageDummy}
+                  component={IntroductionPage}
                 />
                 <Route path={routes.START} component={StartPage} />
 
