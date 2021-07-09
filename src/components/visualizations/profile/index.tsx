2954fc52deca716bc0d36add4433c15f5e0b5f8d
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2021-07-09 16:18:41 +0200

    implement Explainer in Profile Dashboard

--- src/components/visualizations/profile/index.tsx
+++ src/components/visualizations/profile/index.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import { faIdCard } from '@fortawesome/pro-light-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 import { ScrapingResult } from '../../../db/types';
+import Explainer from '../../Explainer';
+import Infobox from '../../Infobox';
 import Beeswarm from './Beeswarm';
 import { useData } from './useData';
 
@@ -24,55 +28,89 @@ export default function StatisticsChart({
 }: {
   data: Array<ScrapingResult>;
 }) {
+  const [explainerIsOpen, setExplainerIsOpen] = useState(true);
   const db = useData(data);
 
   if (!db.history) return null;
 
   return (
-    <div className="cursor-default">
-      <div className="m-7 mt-0 grid grid-cols-8 gap-4">
-        <Badge title="Zeitraum" value={db.days} unit="Tage" />
-        <Badge title="Videos" value={db.history?.length} unit="" />
-        <Badge
-          title="geschaut insgesamt"
-          value={Math.round(db.watchTime)}
-          unit="Minuten"
-        />
-        <Badge
-          title="Ø Videolänge"
-          value={Math.round(db.watchPercentAverage)}
-          unit="%"
-        />
-        <Badge
-          title="Ø Minuten pro Video"
-          value={Math.round(db.watchTimeAverage)}
-          unit="minutes"
-        />
+    <>
+      <Explainer
+        isOpen={explainerIsOpen}
+        onIsOpenChange={(val: boolean) => setExplainerIsOpen(val)}
+      >
+        <FontAwesomeIcon icon={faIdCard} size="6x" className="text-blue-200" />
+        <div className="max-w-prose">
+          <h1 className="hl-4xl my-6">Mein YouTube-Profil</h1>
+          <div className="space-y-4">
+            <p>
+              Jemand musste Josef K. verleumdet haben, denn ohne dass er etwas
+              Böses getan hätte, wurde er eines Morgens verhaftet. »Wie ein
+              Hund!« sagte er, es war, als sollte die Scham ihn überleben.
+            </p>
+            <p>
+              Jemand musste Josef K. verleumdet haben, denn ohne dass er etwas
+              Böses getan hätte, wurde er eines Morgens verhaftet. »Wie ein
+              Hund!« sagte er, es war, als sollte die Scham ihn überleben.
+            </p>
+            <div>
+              <Infobox>
+                <p>
+                  Jemand musste Josef K. verleumdet haben, denn ohne dass er
+                  etwas Böses getan hätte, wurde er eines Morgens verhaftet.
+                  »Wie ein Hund!« sagte er, es war, als sollte die Scham ihn
+                  überleben.
+                </p>
+              </Infobox>
+            </div>
+          </div>
+        </div>
+      </Explainer>
+      <div className="cursor-default">
+        <div className="m-7 mt-0 grid grid-cols-8 gap-4">
+          <Badge title="Zeitraum" value={db.days} unit="Tage" />
+          <Badge title="Videos" value={db.history?.length} unit="" />
+          <Badge
+            title="geschaut insgesamt"
+            value={Math.round(db.watchTime)}
+            unit="Minuten"
+          />
+          <Badge
+            title="Ø Videolänge"
+            value={Math.round(db.watchPercentAverage)}
+            unit="%"
+          />
+          <Badge
+            title="Ø Minuten pro Video"
+            value={Math.round(db.watchTimeAverage)}
+            unit="minutes"
+          />
 
-        <Badge
-          title="Kategoriefavorit"
-          value={db.mostWatchedCategoriesTime[0][0]}
-          small
-          unit=""
-        />
-        <Badge title="Abonnierte Kanäle" value={db.channels.length} unit="" />
-        {/* <Badge
+          <Badge
+            title="Kategoriefavorit"
+            value={db.mostWatchedCategoriesTime[0][0]}
+            small
+            unit=""
+          />
+          <Badge title="Abonnierte Kanäle" value={db.channels.length} unit="" />
+          {/* <Badge
           title="width notifications"
           value={db.channelsNotification.length}
           unit="channels"
         /> */}
-        {db.topChannel && (
-          <Badge
-            title="Kanalfavorit"
-            value={db.topChannel.name}
-            small
-            unit=""
-          />
-        )}
+          {db.topChannel && (
+            <Badge
+              title="Kanalfavorit"
+              value={db.topChannel.name}
+              small
+              unit=""
+            />
+          )}
+        </div>
+        <div className="m-7 mb-0 text-xs text-yellow-1300 p-2 shadow rounded-lg backdrop-filter backdrop-opacity-80 backdrop-contrast-125 backdrop-brightness-110 backdrop-saturate-200">
+          <Beeswarm data={db.history} />
+        </div>
       </div>
-      <div className="m-7 mb-0 text-xs text-yellow-1300 p-2 shadow rounded-lg backdrop-filter backdrop-opacity-80 backdrop-contrast-125 backdrop-brightness-110 backdrop-saturate-200">
-        <Beeswarm data={db.history} />
-      </div>
-    </div>
+    </>
   );
 }
