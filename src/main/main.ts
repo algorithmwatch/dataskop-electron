e42cfa427ea5c07814b730eecef690a6f2cc6bfa
Johannes Filter <hi@jfilter.de>
2022-08-10 18:44:51 +0200

    Fix wrong preload.js location

--- src/main/main.ts
+++ src/main/main.ts
@@ -126,7 +126,9 @@ const createWindow = async () => {
     maxHeight: 1080,
     icon: getAssetPath('icon.png'),
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: app.isPackaged
+        ? path.join(__dirname, 'preload.js')
+        : path.join(__dirname, '../../.erb/dll/preload.js'),
       backgroundThrottling: false,
       sandbox: false,
     },
