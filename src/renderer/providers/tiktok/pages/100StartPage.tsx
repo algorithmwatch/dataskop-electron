66f48cca95c0073ddf2a852c76930f7b3fdd2b94
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2022-08-16 15:30:54 +0200

    start page WIP

--- src/renderer/providers/tiktok/pages/100StartPage.tsx
+++ src/renderer/providers/tiktok/pages/100StartPage.tsx
@@ -12,13 +12,13 @@ import { useNavigation } from '../../../contexts';
 // import FooterNav, {
 //   FooterNavItem,
 // } from '../../youtube/components/FooterNav';
-import awlogo from '../../../static/images/logos/aw-logo.png';
-import bmbflogo from '../../../static/images/logos/bmbf-logo.png';
-import dslogo from '../../../static/images/logos/dslogo.svg';
-import enslogo from '../../../static/images/logos/ens-logo.png';
-import fhplogo from '../../../static/images/logos/fhp-logo.png';
-import mplogo from '../../../static/images/logos/mp-logo.png';
-import uplogo from '../../../static/images/logos/up-logo.png';
+// import awlogo from '../../../static/images/logos/aw-logo.png';
+// import bmbflogo from '../../../static/images/logos/bmbf-logo.png';
+// import dslogo from '../../../static/images/logos/dslogo.svg';
+// import enslogo from '../../../static/images/logos/ens-logo.png';
+// import fhplogo from '../../../static/images/logos/fhp-logo.png';
+// import mplogo from '../../../static/images/logos/mp-logo.png';
+// import uplogo from '../../../static/images/logos/up-logo.png';
 
 export default function StartPage(): JSX.Element {
   const { getNextPage } = useNavigation();
@@ -38,10 +38,10 @@ export default function StartPage(): JSX.Element {
   return (
     <>
       <div className="mx-auto flex flex-col h-full">
-        <div className="flex-grow flex items-center max-h-96">
+        {/* <div className="flex-grow flex items-center max-h-96">
           <img src={dslogo} alt="Dataskop Logo" className="w-80 mx-auto" />
-        </div>
-        <div className="bg-yellow-100">
+        </div> */}
+        {/* <div className="bg-yellow-100">
           <div className="text-center">
             <div className="font-bold mb-3">
               TIK TOK TIK TOK TIK TOK TODO: Partner:
@@ -78,7 +78,7 @@ export default function StartPage(): JSX.Element {
             <div className="font-bold">Gefördert durch:</div>
             <img src={bmbflogo} alt="" className="block w-52 mx-auto -mt-1" />
           </div>
-        </div>
+        </div> */}
       </div>
       <FooterNav items={footerNavItems} />
     </>
