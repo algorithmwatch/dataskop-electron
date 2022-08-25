fe99703dcd5562252f7c3fe55d37d9d0f315e38c
Gregor Weichbrodt <gambolputty@users.noreply.github.com>
2022-08-25 16:10:54 +0100

    implement content container

---
+++ src/renderer/providers/tiktok/components/Content.tsx
@@ -0,0 +1,35 @@
+import { IconDefinition } from "@fortawesome/pro-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import clsx from "clsx";
+import { ReactNode } from "react";
+
+const themes = {
+  gray: "bg-neutral-200/80 rounded-3xl",
+};
+
+export default function Content({
+  title,
+  icon,
+  theme = "gray",
+  children,
+}: {
+  title: string;
+  icon: IconDefinition;
+  theme?: keyof typeof themes;
+  children: ReactNode;
+}) {
+  return (
+    <div
+      className={clsx(
+        "grow max-h-[75vh] p-6 w-5/6 overflow-hidden flex flex-col items-center justify-center",
+        themes[theme],
+      )}
+    >
+      <div className="">
+        <FontAwesomeIcon icon={icon} className={clsx("w-[5rem] h-[5rem]")} />
+        <h1 className="hl-4xl my-7">{title}</h1>
+        <div className="text-xl max-w-prose">{children}</div>
+      </div>
+    </div>
+  );
+}
